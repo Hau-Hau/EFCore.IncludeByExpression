b3b610d59981c9a3d470c187b018560bf9b94a17
piotr.a.michalewicz@gmail.com <piotr.a.michalewicz@gmail.com>
2024-06-29 19:21:30 +0200

    Extract abstractions to project

---
+++ EFCore.IncludeByExpression.Abstractions/IIncludeable.cs
@@ -0,0 +1,13 @@
+﻿namespace EFCore.IncludeByExpression.Abstractions
+{
+    /// <summary>
+    ///     Represents an interface that serves as a marker for queryable types
+    ///     where navigation properties can be explicitly included.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of entity being queried.</typeparam>
+    public interface IIncludable<TEntity>
+        where TEntity : class
+    {
+        // Serves as a marker for queryable types that support the inclusion of navigation properties.
+    }
+}
---
+++ EFCore.IncludeByExpression.Abstractions/IThenIncludable.cs
@@ -0,0 +1,14 @@
+﻿namespace EFCore.IncludeByExpression.Abstractions
+{
+    /// <summary>
+    ///     Represents an interface that extends the functionality of <see cref="IIncludable{TEntity}" />,
+    ///     allowing the chaining of navigation property inclusions in a query for a given entity type.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of entity being queried.</typeparam>
+    /// <typeparam name="TProperty">The type of the related entity to be included.</typeparam>
+    public interface IThenIncludable<TEntity, out TProperty> : IIncludable<TEntity>
+        where TEntity : class
+    {
+        // Serves as a marker for queryable types that support the chaining of navigation property inclusions.
+    }
+}
